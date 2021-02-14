7d77cf1fc720c9b1475b8920db4e0bf64ec5ce46
saraluk <saralukkai@gmail.com>
2021-02-14 17:21:25 -0500

    change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: "Full-Stack Bootcamp!",
+    title: "Gatsby Bootcamp!",
     author: "Saraluk Kaiwansakul",
   },
   plugins: [
